509dc2f07997f338e6e9194071bb42c0bb525080
변우진 <sforot9@gmail.com>
2021-08-11 16:28:06 +0900

    fix : camera design 변경
    - 카메라 크기 확장
    - 촬영 버튼 색상 변경

--- src/screens/ExploreScreen.js
+++ src/screens/ExploreScreen.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import {View, Text, TouchableOpacity, Image} from 'react-native';
 import {RNCamera, FaceDetector} from 'react-native-camera';
 import CameraRoll from '@react-native-community/cameraroll';
+import LinearGradient from 'react-native-linear-gradient';
 
 const ExploreScreen = () => {
   // for data to  send api
@@ -79,7 +80,7 @@ const ExploreScreen = () => {
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
           <RNCamera
             ref={cameraRef}
-            style={{marginTop: '10%', width: '80%', height: '75%'}}
+            style={{width: '100%', height: '100%'}}
             captureAudio={false}
             type={RNCamera.Constants.Type.back}
             flashMode={RNCamera.Constants.FlashMode.on}
@@ -96,24 +97,28 @@ const ExploreScreen = () => {
               buttonNegative: 'Cancel',
             }}
           />
-          <View
-            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <View style={{position: 'absolute', bottom: 15}}>
             <TouchableOpacity
               onPress={async () => {
                 await takePhoto();
                 await getPhotos();
                 // post api
                 console.log('hello');
-              }}
-              style={{
-                width: 50,
-                height: 50,
-                borderRadius: 50,
-                border: 'gray',
-                borderWidth: 1,
-                backgroundColor: 'pink',
-              }}
-            />
+              }}>
+              <LinearGradient
+                start={{x: 0, y: 0}}
+                end={{x: 0, y: 1}}
+                colors={[
+                  'rgba(255, 113, 113, 0.45)',
+                  'rgba(177, 108, 246, 0.45)',
+                ]}
+                style={{
+                  width: 55,
+                  height: 55,
+                  borderRadius: 55,
+                }}
+              />
+            </TouchableOpacity>
           </View>
         </View>
       </>
