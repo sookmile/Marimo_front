ef388192caa60f1afbcbe8aa9c68fea7211f6bcd
변우진 <sforot9@gmail.com>
2021-09-11 17:31:05 +0900

    feat : social login & speech to text

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -1,28 +1,71 @@
-import * as React from 'react';
-import {
-  View,
-  Text,
-  Stylesheet,
-  Button,
-  Image,
-  TouchableOpacity,
-} from 'react-native';
-import SpellingGame from './SpellingGame';
+import React, {useState, useEffect} from 'react';
+import Styled from 'styled-components/native';
+import Voice from 'react-native-voice';
+import {Text} from 'react-native';
 
-const GameScreen = ({navigation}) => {
+const Container = Styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #f5fcff;
+`;
+const ButtonRecord = Styled.Button``;
+const VoiceText = Styled.Text`
+  margin: 32px;
+`;
+
+const GameScreen = () => {
+  const [isRecord, setIsRecord] = useState(false);
+  const [text, setText] = useState('');
+  const buttonLabel = isRecord ? 'Stop' : 'Start';
+  const voiceLabel = text
+    ? text
+    : isRecord
+    ? 'Say something...'
+    : 'press Start button';
+
+  const _onSpeechStart = () => {
+    console.log('onSpeechStart');
+    setText('');
+  };
+  const _onSpeechEnd = () => {
+    console.log('onSpeechEnd');
+  };
+  const _onSpeechResults = event => {
+    console.log('onSpeechResults');
+    setText(event.value[0]);
+  };
+  const _onSpeechError = event => {
+    console.log('_onSpeechError');
+    console.log(event.error);
+  };
+
+  const _onRecordVoice = () => {
+    if (isRecord) {
+      Voice.stop();
+    } else {
+      Voice.start('ko-KR');
+    }
+    setIsRecord(!isRecord);
+  };
+
+  useEffect(() => {
+    Voice.onSpeechStart = _onSpeechStart;
+    Voice.onSpeechEnd = _onSpeechEnd;
+    Voice.onSpeechResults = _onSpeechResults;
+    Voice.onSpeechError = _onSpeechError;
+
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
+
   return (
-    <View style={{flex: 1, zIndex: -1, display: 'flex'}}>
-      <TouchableOpacity onPress={() => navigation.navigate('SpellingGame')}>
-        <Image
-          style={{
-            width: '100%',
-            height: '100%',
-          }}
-          source={require('../assets/game(ver3).png')}
-          resizeMode="cover"
-        />
-      </TouchableOpacity>
-    </View>
+    <Container>
+      <VoiceText>{voiceLabel}</VoiceText>
+      <Text>{text ? `${text}` : '단어 없음..'}</Text>
+      <ButtonRecord onPress={_onRecordVoice} title={buttonLabel} />
+    </Container>
   );
 };
 
